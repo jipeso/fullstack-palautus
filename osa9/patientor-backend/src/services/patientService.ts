5c8576cfac7c9ca460797d07b34cdd489c5b7c7d
jipeso <149601375+jipeso@users.noreply.github.com>
2025-10-23 19:37:39 +0300

    part 9 exercises 8-13

---
+++ osa9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,33 @@
+import { v4 as uuid } from 'uuid';
+import { Patient, NonSensitivePatient, NewPatient } from '../types';
+import patients from '../../data/patients';
+
+const getPatients = (): Patient[] => {
+    return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+    return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation
+    }));
+};
+
+const addPatient = ( entry: NewPatient ): Patient => {
+    const newPatient = {
+        id: uuid(),
+        ...entry
+    };
+
+    patients.push(newPatient);
+    return newPatient;
+};
+
+export default {
+    getPatients,
+    getNonSensitivePatients,
+    addPatient
+};
